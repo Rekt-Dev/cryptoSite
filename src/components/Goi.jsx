2d3178c5d2bb683a7aeb9a24c8bdc748db5ba5ca
rektDev <rektdevvv@gmail.com>
2022-09-22 06:50:04 +0000

    Update 6 files

---
+++ src/components/Goi.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+export const Goi=()=>{
+    
+    return(<div>
+        Global open interest Component
+    </div>
+    )
+}
---
+++ src/components/Mcap.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+export const Mcap=()=>{
+    
+    return(<div>
+        MarketCaps Component
+    </div>
+    )
+}
