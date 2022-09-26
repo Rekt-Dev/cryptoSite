2bb634f54a8aaef9db9cb58149a3dc197a42b125
rektDev <rektdevvv@gmail.com>
2022-09-26 17:18:53 +0000

    Update 3 files

--- src/services/getData.js
+++ src/services/getData.js
@@ -1,9 +1,10 @@
-export function getData(){
+export async function getData(){
 
-fetch(`https://api.cryptonator.com/api/ticker/btc-usd`)
+
+
+await fetch('https://api.cryptonator.com/api/ticker/btc-usd')
   .then(response => response.json())
   .then(data => {
     console.log(data)
   })
-  .catch(err => console.log(err))
-}
+  .catch(err => console.log(`this is the error mofo : ${err}`))}
