f24021c1354833f78f21f99d3664d298a73603bb
rektDev <rektdevvv@gmail.com>
2022-09-29 14:29:20 +0000

    Update querySvr.js

--- src/services/querySvr.js
+++ src/services/querySvr.js
@@ -1,35 +1,34 @@
+export async function querySvr() {
+  const corsAnywhere = "https://cors-anywhere.herokuapp.com/";
 
-export async function querySvr (){
-    
-
-    const corsAnywhere = "https://cors-anywhere.herokuapp.com/";
-
-const apiKey = "ae892c4004msh44f9eb6e9b4441cp13282cjsn9e8bfa4b5636";
-const url = `${corsAnywhere}https://api.coinranking.com/v2/coins`;
-const queryString = new URLSearchParams({
-  "x-access-token": apiKey,
-  search: "Bit",
-});
-
-let response = await fetch(`${url}?${queryString}`)
-  .then((response) => response.json())
-  .then((response) => {
-    if (response.status === "success") {
-      console.log("fetch success", `${response}`);
-      const html = response.data.coins.map(
-        (coin) => `
-        <tr>
-          <td>${coin.rank}</td>
-          <td>${coin.name}</td>
-          <td>${coin.price}</td>
-        </tr>
-      `
-      );
-    } else {
-      `<tr><td colspan="3">${response.message}</td></tr>`;
-    }
-  })
-  .catch((error) => {
-    console.error(error);
+  const apiKey = "ae892c4004msh44f9eb6e9b4441cp13282cjsn9e8bfa4b5636";
+  const url = `${corsAnywhere}https://api.coinranking.com/v2/coins`;
+  const queryString = new URLSearchParams({
+    "x-access-token": apiKey,
+    search: "Bit",
   });
-}
+
+  await fetch(`${url}?${queryString}`)
+    .then((response) => response.json())
+    .then((response) => {
+      if (response.status === "success") {
+        console.log("fetch success", response);
+        const html = response.data.coins;
+return ({html?
+       html.map((coin,index) 
+          => (<div>
+              <ul>
+          <div>${{coin}=coin.rank,key={index}}</div>
+          <div>${coin.name}</div>
+          <div>${coin.price}</div>
+        </ul>
+        </div> 
+          
+})
+    
+      
+   
+    .catch((error) => {
+      console.error(error);
+
+    }
