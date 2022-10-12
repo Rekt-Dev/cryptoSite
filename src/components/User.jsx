c547224478d56e65f3543f8c8c9bec1fc8feb8fa
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-10-12 17:10:26 +0300

    ......jhm,

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,4 +1,73 @@
 import { useState } from "react";
+import styles from "./LoginForm.module.css";
+
+const User = (props) => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+    confirmPassword: ""
+  });
+
+  const onUpdateField = (e) => {
+    const nextFormState = {
+      ...form,
+      [e.target.name]: e.target.value
+    };
+    setForm(nextFormState);
+  };
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    alert(JSON.stringify(form, null, 2));
+  };
+
+  return (
+    <form className={styles.form} onSubmit={onSubmitForm}>
+      <div className={styles.formGroup}>
+        <label className={styles.formLabel}>Email</label>
+        <input
+          className={styles.formField}
+          type="text"
+          aria-label="Email field"
+          name="email"
+          value={form.email}
+          onChange={onUpdateField}
+        />
+      </div>
+      <div className={styles.formGroup}>
+        <label className={styles.formLabel}>Password</label>
+        <input
+          className={styles.formField}
+          type="password"
+          aria-label="Password field"
+          name="password"
+          value={form.password}
+          onChange={onUpdateField}
+        />
+      </div>
+      <div className={styles.formGroup}>
+        <label className={styles.formLabel}>Confirm Password</label>
+        <input
+          className={styles.formField}
+          type="password"
+          aria-label="Confirm password field"
+          name="confirmPassword"
+          value={form.confirmPassword}
+          onChange={onUpdateField}
+        />
+      </div>
+      <div className={styles.formActions}>
+        <button className={styles.formSubmitBtn} type="submit">
+          Login
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default User;
+
+/* import { useState } from "react";
 import "../styles.css";
 
 export const User = () => {
@@ -37,3 +106,4 @@ export const User = () => {
     </div>
   );
 };
+ */
