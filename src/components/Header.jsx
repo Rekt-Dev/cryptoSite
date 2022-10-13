f725fb9bf38f8d43717ada8477aed5f513a1225a
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-10-13 17:44:02 +0300

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,12 @@
-import React from "react"
-    import "../styles.css"
+import React from "react";
+import "../styles.css";
 
-
-export const Header=()=>{
-    return(<div>
-        
-        <div className="Header">
-            
-        <h1>cryptost8</h1>
+export const Header = () => {
+  return (
+    <div>
+      <div className="Header">
+        <h1>cryptoSt8</h1>
+      </div>
     </div>
-    
-    </div>
-    )
-}
+  );
+};
