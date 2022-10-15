017f31d47f78d81a77cd5daecbe7b774aaace55a
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-10-15 16:07:20 +0300

    .

--- src/components/PriceList.jsx
+++ src/components/PriceList.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "../styles.css";
 
-export const PriceList = () => {
+export const PriceList = (props) => {
   return (
     <div>
       <div className="PriceList">
         <h1>Price list component </h1>
-        <div></div>
+        <div>
+          {jsonData.data
+            ? jsonData.data.map((artWork, index) => (
+                <Card key={index} item={artWork} />
+              ))
+            : "No data to show"}
+        </div>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { BrowserRouter, Link, Switch } from "react-router-dom";
+import { PriceList } from "./components/PriceList";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="pricelist" element={<PriceList />} />
+      </Routes>
+    </Router>
   </StrictMode>
 );
