d894908274be91b12b892d54f9c21d74c1498825
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-10-24 19:50:02 +0300

    .

--- src/components/MarketData.jsx
+++ src/components/MarketData.jsx
@@ -5,8 +5,10 @@ export const MarketData = (props) => {
     <div>
       Market Data component
       <div></div>
-      <div>2</div>
-      <div>3</div>
+      <div>
+        <button>refresh something</button>
+      </div>
+      <div>3 </div>
       <div>4</div>
       <div>5</div>
       <div>6</div>
